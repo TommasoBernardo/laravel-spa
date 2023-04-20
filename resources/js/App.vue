96088cfa865f22104cacce51052ac627c800ca3b
TommasoBernardo <tommasob1210@gmail.com>
2023-04-20 15:57:24 +0200

    update navbar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,23 +1,32 @@
 <template>
-    <div class="container">
-        <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navl"
-                aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="navbar-nav" v-if="isLoggedIn">
-                <router-link to="/dashboard">dashboard</router-link>
-                <a class="nav-item nav-link" @click="logout">Logout</a>
-            </div>
-            <div class="navbar-nav" v-else>
-                <router-link to="/">Home</router-link>
-                <router-link to="/login">Login</router-link>
-                <router-link to="/register">Register</router-link>
-            </div>
-        </nav>
+            <div class="collapse navbar-collapse" id="navbarNav">
 
-        <router-view></router-view>
-    </div>
+                <div class="navbar-nav" v-if="isLoggedIn">
+                    <router-link to="/dashboard">dashboard</router-link>
+                    <a class="nav-item nav-link" @click="logout">Logout</a>
+                </div>
+                <ul class="navbar-nav" v-else>
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/login" class="nav-link">Login</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/register" class="nav-link">Register</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <router-view></router-view>
 </template>
 
 <script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
 </head>
 
 <body>
@@ -20,14 +22,15 @@
         </script>
     @else
         <script>
-            window.Laravel = {
-                !!json_encode({
-                    'isLoggedin' => false
-                })
-            }
+            window.Laravel = {!! json_encode([
+                'isLoggedin' => false,
+            ]) !!}
         </script>
     @endif
     <div id="app"></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
     <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
 </body>
 
